99cf3b8621ad7ed2ab041d38ca9d07ec1be1982e
Shobit Shrivastav <Shobit_Shrivastav@mckinsey.com>
2020-05-17 21:03:12 +0530

    initial commit

---
+++ src/main/java/com/demo/api/SpringAndReactApplication.java
@@ -0,0 +1,12 @@
+package com.demo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndReactApplication.class, args);
+	}
+}
